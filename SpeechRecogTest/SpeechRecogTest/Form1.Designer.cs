bf51cb84283ef62b6711b9b792e83d1c5ccf851b
rook13 <rook3dev@gmail.com>
2013-05-19 17:20:27 +0100

    Adding Speech Recognition Test Project

---
+++ SpeechRecogTest/SpeechRecogTest/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace SpeechRecogTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.enableButton = new System.Windows.Forms.Button();
+            this.disableButton = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.enableButton, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.disableButton, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtOutput, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.21374F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 87.78626F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 262);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // enableButton
+            // 
+            this.enableButton.AutoSize = true;
+            this.enableButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.enableButton.Location = new System.Drawing.Point(3, 3);
+            this.enableButton.Name = "enableButton";
+            this.enableButton.Size = new System.Drawing.Size(136, 25);
+            this.enableButton.TabIndex = 0;
+            this.enableButton.Text = "Enable Speech";
+            this.enableButton.UseVisualStyleBackColor = true;
+            this.enableButton.Click += new System.EventHandler(this.enableButton_Click);
+            // 
+            // disableButton
+            // 
+            this.disableButton.AutoSize = true;
+            this.disableButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.disableButton.Location = new System.Drawing.Point(145, 3);
+            this.disableButton.Name = "disableButton";
+            this.disableButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.disableButton.Size = new System.Drawing.Size(136, 25);
+            this.disableButton.TabIndex = 1;
+            this.disableButton.Text = "Disable Speech";
+            this.disableButton.UseVisualStyleBackColor = true;
+            this.disableButton.Click += new System.EventHandler(this.disableButton_Click);
+            // 
+            // txtOutput
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.txtOutput, 2);
+            this.txtOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtOutput.Location = new System.Drawing.Point(3, 34);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(278, 225);
+            this.txtOutput.TabIndex = 2;
+            this.txtOutput.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button enableButton;
+        private System.Windows.Forms.Button disableButton;
+        private System.Windows.Forms.RichTextBox txtOutput;
+    }
+}
+
---
+++ SpeechRecogTest/SpeechRecogTest/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Threading;
+
+namespace SpeechRecogTest
+{
+    public partial class Form1 : Form
+    {
+
+        public SpeechRecognitionEngine recognitionEngine;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            recognitionEngine = new SpeechRecognitionEngine();
+            recognitionEngine.SetInputToDefaultAudioDevice();
+
+            recognitionEngine.SpeechRecognized += (s, args) =>
+            {
+                foreach (RecognizedWordUnit word in args.Result.Words)
+                {
+                   txtOutput.Text += word.Text + " ";
+                }
+                txtOutput.Text += Environment.NewLine;
+            };
+
+            recognitionEngine.LoadGrammar(new DictationGrammar());
+
+        }
+
+        private void enableButton_Click(object sender, EventArgs e)
+        {
+            recognitionEngine.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        private void disableButton_Click(object sender, EventArgs e)
+        {
+            recognitionEngine.RecognizeAsyncStop();
+        }
+
+        
+    }
+}
